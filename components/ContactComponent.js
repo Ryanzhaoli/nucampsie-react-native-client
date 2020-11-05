24674edbc2bd0860a9a3767b329768da63fa2168
Ryan <jijizaza999@gmail.com>
2020-11-04 18:31:33 -0800

    second commit

---
+++ components/ContactComponent.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { Text } from 'react-native'
+import { Card, Button, Icon } from 'react-native-elements'
+import { ScrollView } from 'react-native-gesture-handler'
+import * as Animatable from 'react-native-animatable'
+import * as MailComposer from "expo-mail-composer"
+
+class ContactComponent extends Component {
+  static navigationOptions = {
+    title: "Contact Us",
+  }
+
+  sendMail() {
+    MailComposer.composeAsync({
+      recipients: ["campsites@nucamp.co"],
+      subject: "Inquiry",
+      body: "To whom it may concern:",
+    })
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+          <Card wrapperStyle={{ margin: 20 }} title='Contact Information'>
+            <Text style={{ marginBottom: 10 }}>
+              1 Nucamp Way{"\n"}
+              Seattle, WA 98001{"\n"}
+              U.S.A.
+            </Text>
+            <Text>Phone: 1-206-555-1234</Text>
+            <Button
+              title='Send Email'
+              buttonStyle={{ backgroundColor: "#5637DD", margin: 40 }}
+              icon={
+                <Icon
+                  name='envelope-o'
+                  type='font-awesome'
+                  color='#fff'
+                  iconStyle={{ marginRight: 10 }}
+                />
+              }
+              onPress={() => this.sendMail()}
+            />
+          </Card>
+        </Animatable.View>
+      </ScrollView>
+    )
+  }
+}
+
+export default ContactComponent
---
+++ shared/baseUrl.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'http://192.168.116.244:3000/';
+
+ // json-server -H 0.0.0.0 --watch db.json -p 3000 -d 2000
